59a17e41008a78ae884fa2dbe6a4b46066a03475
Sarthak Rana <rsarthak2845@gmail.com>
2024-12-20 23:11:05 +0530

    added filter functionality

--- script.js
+++ script.js
@@ -1,57 +1,75 @@
-
 const countryContainer = document.querySelector(".countries-container");
 const darkModeToggle = document.querySelector(".dark-mode-btn");
-
+const filterRegion = document.querySelector(".filter-by-region");
 fetch("https://restcountries.com/v3.1/all")
   .then((res) => res.json())
   .then((data) => {
-    data.forEach((country) => {
-
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-    countryCard.href = `/country.html?name=${country.name.common}`
-    // countryCard.target="_blank"
-      const cardHtml = `
-        <img src="${country.flags.svg}" alt="${country.name.common} flag">
-        <div class="card-text">
-          <h3 class="card-title">${country.name.common}</h3>
-          <p><b>Population: </b>${country.population.toLocaleString()}</p>
-          <p><b>Region: </b>${country.region}</p>
-          <p><b>Capital: </b>${country.capital ? country.capital[0] : "N/A"}</p>
-        </div>`;    
-      countryCard.innerHTML = cardHtml;
-      countryContainer.appendChild(countryCard);
-    });
+ renderCards(data)
   })
   .catch((error) => {
     console.error("Error fetching countries data:", error);
     countryContainer.innerHTML = "<p>Failed to load countries data.</p>";
   });
 
- 
+filterRegion.addEventListener("change", (e) =>{
+  if(e.target.value === 'filter'){
+    fetch("https://restcountries.com/v3.1/all").then((res) => res.json())
+    .then((data) =>{
+  renderCards(data)
+    })
+     }
+     
+     else{
+  fetch(`https://restcountries.com/v3.1/region/${e.target.value}`).then((res) => res.json())
+  .then((data) =>{
+renderCards(data)
+  })
+}
+} )
 
-  
-  darkModeToggle.addEventListener("click", () => {
-
-    document.body.classList.toggle("dark-mode-body");
-    document.querySelector(".header-container").classList.toggle("dark-mode-header");
-  
- 
-    const countryCards = document.querySelectorAll(".country-card");
-    countryCards.forEach(card => {
-      card.classList.toggle("dark-mode-card");
-      console.log(card);
-    });
-  
-    const icon = darkModeToggle.querySelector("i");
-    if (document.body.classList.contains("dark-mode-body")) {
-      icon.classList.replace("bi-moon-fill", "bi-sun-fill");
-      darkModeToggle.innerHTML = `<i class="bi bi-sun-fill"></i>&nbsp;&nbsp;Light Mode`;
-    } else {
-      icon.classList.replace("bi-sun-fill", "bi-moon-fill");
-      darkModeToggle.innerHTML = `<i class="bi bi-moon-fill"></i>&nbsp;&nbsp;Dark Mode`;
-    }
+function renderCards(data){
+  countryContainer.innerHTML=''
+  data.forEach((country) => {
+    console.log(country)
+    const countryCard = document.createElement("a");
+    countryCard.classList.add("country-card");
+    countryCard.href = `/country.html?name=${country.name.common}`
+    // countryCard.target="_blank"
+    const cardHtml = `
+      <img src="${country.flags.svg}" alt="${country.name.common} flag">
+      <div class="card-text">
+        <h3 class="card-title">${country.name.common}</h3>
+        <p><b>Population: </b>${country.population.toLocaleString()}</p>
+        <p><b>Region: </b>${country.region}</p>
+        <p><b>Capital: </b>${country.capital ? country.capital[0] : "N/A"}</p>
+      </div>`;
+    countryCard.innerHTML = cardHtml;
+    countryContainer.appendChild(countryCard);
   });
-  
-  
-  
+}
+
+
+
+darkModeToggle.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode-body");
+  document.querySelector(".header-container").classList.toggle("dark-mode-header");
+
+
+  const countryCards = document.querySelectorAll(".country-card");
+  countryCards.forEach(card => {
+    card.classList.toggle("dark-mode-card");
+    console.log(card);
+  });
+
+  const icon = darkModeToggle.querySelector("i");
+  if (document.body.classList.contains("dark-mode-body")) {
+    icon.classList.replace("bi-moon-fill", "bi-sun-fill");
+    darkModeToggle.innerHTML = `<i class="bi bi-sun-fill"></i>&nbsp;&nbsp;Light Mode`;
+  } else {
+    icon.classList.replace("bi-sun-fill", "bi-moon-fill");
+    darkModeToggle.innerHTML = `<i class="bi bi-moon-fill"></i>&nbsp;&nbsp;Dark Mode`;
+  }
+});
+
+
